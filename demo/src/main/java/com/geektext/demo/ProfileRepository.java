181d4a18f0d1a053c7bee6e13adbf809eff5d2e9
dotKiiro <tx.andres.ramirez@gmail.com>
2021-07-18 17:47:58 -0400

    Changes to profile files

--- demo/src/main/java/com/geektext/demo/ProfileRepository.java
+++ demo/src/main/java/com/geektext/demo/ProfileRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProfileRepository extends CrudRepository<Profile, String> {
 
-    Profile findByEmail(String email);
+    //Profile findByEmail(String email);
 
 }
--- demo/src/main/java/com/geektext/demo/RegisterController.java
+++ demo/src/main/java/com/geektext/demo/RegisterController.java
@@ -2,16 +2,35 @@ package com.geektext.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
-@RequestMapping(value="/register")
+@RequestMapping("/register")
 
 public class RegisterController {
     @Autowired
     private ProfileRepository profileRepo;
 
-    @PostMapping(path="/new")
+    /*@GetMapping(path="/register")
+    public @ResponseBody String registerForm()
+    {
+
+        return "register";
+    }
+
+    @PostMapping(path="/register")
+    public String registerSubmit(@ModelAttribute Profile profile, Model model)
+    {
+        model.addAttribute("register", profile);
+        return "registrationResult";
+    } */
+
+
     public @ResponseBody String addNewProfile (@RequestParam String fname, @RequestParam String lname, @RequestParam
                                                String email, @RequestParam String address, @RequestParam String phone,
                                                @RequestParam String password)
@@ -22,6 +41,9 @@ public class RegisterController {
         user.setEmail(email);
         user.setAddress(address);
         user.setPhone(phone);
+        user.setPassword(password);
+        profileRepo.save(user);
+        return "register";
 
     }
 }
